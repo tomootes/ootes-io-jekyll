cf267f85201a4fd7ff8a719d86d83282f3702928
Tom Ootes <tom@ootes.io>
2025-01-28 13:55:24 +0100

    add jekyll and tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    './_layouts/**/*.html',
+    './_includes/**/*.html',
+    './_posts/**/*.md',
+    './assets/css/**/*.css',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
